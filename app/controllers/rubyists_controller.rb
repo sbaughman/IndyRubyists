b7749199c66cd9d4dfef28e85b526dc6362b6190
Seth Baughman <sbaughman1@me.com>
2016-04-20 19:30:07 -0400

    add controller action for showing user repos

--- app/controllers/rubyists_controller.rb
+++ app/controllers/rubyists_controller.rb
@@ -3,12 +3,19 @@ class RubyistsController < ApplicationController
   def index
     response = Octokit.search_users("location:Indianapolis language:Ruby", :sort => "joined", :per_page => 100).items
     response.concat Octokit.last_response.rels[:next].get.data.items
+    response.reverse!
     @rubyists = Kaminari.paginate_array(response).page(params[:page]).per(30)
   end
 
   def show
     @user = Octokit.user(params[:username])
     @events = @user.rels[:events].get.data
+  end
+
+  def user_repos
+    @user = Octokit.user(params[:username])
+    repos = @user.rels[:repos].get.data
+    @repos = repos.sort_by { |repo| repo[:stargazers_count] }
   end
 
 end
