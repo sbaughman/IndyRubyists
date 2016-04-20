61d669d60ba5e74f9a906e7fd72dd9bea243552a
Seth Baughman <sbaughman1@me.com>
2016-04-20 16:27:54 -0400

    finish index page, styling, add pagination with kaminari

--- app/controllers/rubyists_controller.rb
+++ app/controllers/rubyists_controller.rb
@@ -1,7 +1,9 @@
 class RubyistsController < ApplicationController
 
   def index
-    @rubyists = Octokit.search_users("location:Indianapolis language:Ruby", :sort => "created").items
+    response = Octokit.search_users("location:Indianapolis language:Ruby", :sort => "joined", :per_page => 100).items
+    response.concat Octokit.last_response.rels[:next].get.data.items
+    @rubyists = Kaminari.paginate_array(response).page(params[:page]).per(30)
   end
 
 end
